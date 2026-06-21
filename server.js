98661f78c6efdeb60d4bf7f9113005e583d3614d
guyka2212 <guykadosh22@gmail.com>
2026-06-21 17:51:27 +0300

    Add npm start script with static file server

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const PUBLIC = __dirname;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(PUBLIC, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      return res.end('<h1>404 Not Found</h1>');
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`Gita site running at http://localhost:${PORT}`);
+});
